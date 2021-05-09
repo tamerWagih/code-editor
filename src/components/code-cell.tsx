bac1546e73556d3368e2b1a84aa41c10e6f39474
Tamer Wagih <mrtamerwagih@gmail.com>
2021-05-09 14:32:59 +0300

    implementing text-editor and code-editor

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import bundle from '../bundler/index';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import Resizable from './resizable';
+
+const CodeCell = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const [err, setErr] = useState('');
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output.code);
+      setErr(output.err);
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
+  return (
+    <Resizable direction="vertical">
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+        <Resizable direction={'horizontal'}>
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
+        <Preview code={code} err={err} />
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
--- src/index.tsx
+++ src/index.tsx
@@ -1,82 +1,12 @@
-import * as esbuild from 'esbuild-wasm';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDom from 'react-dom';
-import { useState, useEffect, useRef } from 'react';
-
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
-
+// import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 const App = () => {
-  const [input, setInput] = useState('');
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
-  const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id='root'></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try{
-              eval(event.data);   
-            } catch(err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              cosole.error(err);
-            }
-          }, false)
-        </script>
-      </body>
-    </html>
-  `;
   return (
     <div>
-      <textarea
-        style={{ margin: '0px', width: '417px', height: '146px' }}
-        onChange={(e) => setInput(e.target.value)}
-        value={input}
-      ></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <iframe
-        ref={iframe}
-        sandbox="allow-scripts"
-        srcDoc={html}
-        title="code preview"
-      />
+      {/* <CodeCell /> */}
+      <TextEditor />
     </div>
   );
 };

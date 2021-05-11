15719fdb2e7a802858c6751c341b56bb15ce005e
Tamer Wagih <mrtamerwagih@gmail.com>
2021-05-11 05:13:38 +0300

    adding cell list, cell list item action bar and add cell components and refactor redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
+import CellList from './components/cell-list';
 import { store } from './state/';
-// import CodeCell from './components/code-cell';
-import TextEditor from './components/text-editor';
+
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        {/* <CodeCell /> */}
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
